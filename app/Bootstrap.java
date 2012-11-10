76772c8b3204e85cd9ee3834805391bb5520a12c
Jacob Tardieu <jacob@tardieu.net>
2012-11-10 10:45:25 +0100

    Commit avant test SVN.

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -23,9 +23,39 @@ public class Bootstrap extends Job {
 			
 			Sandwich s3 = new Sandwich();
 			s3.name = "Sandwich3";
-			s3.description = "Le meilleur des sandwiches";
+			s3.description = "Le meilleur des sandwiches avec du Caviar et autre";
 			s3.prix=2000;
 			s3.save();
+			
+			Sandwich s4 = new Sandwich();
+			s4.name = "Sandwich3";
+			s4.description = "Le meilleur des sandwiches avec du Caviar et autre";
+			s4.prix=2000;
+			s4.save();
+			
+			Sandwich s5 = new Sandwich();
+			s5.name = "Sandwich3";
+			s5.description = "Le meilleur des sandwiches avec du Caviar et autre";
+			s5.prix=2000;
+			s5.save();
+			
+			Sandwich s6 = new Sandwich();
+			s6.name = "Sandwich3";
+			s6.description = "Le meilleur des sandwiches avec du Caviar et autre";
+			s6.prix=2000;
+			s6.save();
+			
+			Sandwich s7 = new Sandwich();
+			s7.name = "Sandwich3";
+			s7.description = "Le meilleur des sandwiches avec du Caviar et autre";
+			s7.prix=2000;
+			s7.save();
+			
+			Sandwich s8 = new Sandwich();
+			s8.name = "Sandwich3";
+			s8.description = "Le meilleur des sandwiches avec du Caviar et autre";
+			s8.prix=2000;
+			s8.save();
 		}
 	}
 }
--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.cache.Cache;
 import play.mvc.*;
 
 import java.util.*;
@@ -9,9 +10,26 @@ import models.*;
 
 public class Application extends Controller {
 
+
     public static void index() {
-    	List<Sandwich> sandwiches = Sandwich.findAll();
+        List<Sandwich> sandwiches = Sandwich.findAll();
+       
+        //panier(null,2);
+       
         render(sandwiches);
     }
+   
+    public static void panier(Sandwich s, int quantite) {
+        HashMap<Sandwich, Integer> panier = (HashMap) Cache.get(session.getId()+"panier");
+        if (panier==null) {
+            panier = new HashMap<Sandwich, Integer>();
+        }
+
+        panier.put(s, quantite);
+        panier.put(new Sandwich("test", "toto", 3), 1);
+        Cache.set(session.getId()+"panier", panier);
+       
+    }
+    
 
 }
